0313affa0908f555bc2bfb540cbee0729df25b59
qounme <qounme@gmail.com>
2024-08-17 22:02:43 +0900

    [issue/5] feat: タグを変更する

--- src/functions/frameworks/react/SinglePageApplicationForm/ConfirmationForm.tsx
+++ src/functions/frameworks/react/SinglePageApplicationForm/ConfirmationForm.tsx
@@ -21,29 +21,29 @@ export const ConfirmationForm = ({
   }, [])
 
   return (
-    <form className="w-full">
-      <label className="form-control">
+    <div className="w-full">
+      <div className="form-control">
         <div className="label">
           <span className="label-text">Email</span>
         </div>
         <p className="input input-bordered flex items-center bg-base-200">{getValues('email')}</p>
         <div className="label"></div>
-      </label>
-      <label className="form-control">
+      </div>
+      <div className="form-control">
         <div className="label">
           <span className="label-text">Gender</span>
         </div>
         <p className="input input-bordered flex items-center bg-base-200">{getValues('gender')}</p>
         <div className="label"></div>
-      </label>
-      <label className="form-control">
+      </div>
+      <div className="form-control">
         <div className="label">
           <span className="label-text">Name</span>
         </div>
         <p className="input input-bordered flex items-center bg-base-200">{getValues('name')}</p>
         <div className="label"></div>
-      </label>
-      <label className="form-control">
+      </div>
+      <div className="form-control">
         <div className="label">
           <span className="label-text">Phone number</span>
         </div>
@@ -53,21 +53,21 @@ export const ConfirmationForm = ({
           </p>
         </div>
         <div className="label"></div>
-      </label>
-      <label className="form-control">
+      </div>
+      <div className="form-control">
         <div className="label">
           <span className="label-text">Bio</span>
         </div>
         <p className="input input-bordered flex items-center bg-base-200">{getValues('bio')}</p>
         <div className="label"></div>
-      </label>
-      <label className="form-control">
+      </div>
+      <div className="form-control">
         <div className="label">
           <span className="label-text">Password</span>
         </div>
         <p className="input input-bordered flex items-center bg-base-200">{'*'.repeat(getValues('password').length)}</p>
         <div className="label"></div>
-      </label>
+      </div>
       <div className="card-actions mt-4 w-full justify-center">
         <button type="button" className="btn btn-neutral btn-block sm:btn-wide" onClick={goToPreviousStep}>
           Back
@@ -80,6 +80,6 @@ export const ConfirmationForm = ({
           Register
         </button>
       </div>
-    </form>
+    </div>
   )
 }
