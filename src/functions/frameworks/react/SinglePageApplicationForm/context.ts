2a4c8daccc90ee8d8b75a06aeb78288003915077
qounme <qounme@gmail.com>
2024-08-24 09:48:47 +0900

    [issue/5] feat: フォーム処理をコンテキストに定義する

--- src/functions/frameworks/react/SinglePageApplicationForm/context.ts
+++ src/functions/frameworks/react/SinglePageApplicationForm/context.ts
@@ -1,19 +1,46 @@
 import { createContext, useState } from 'react'
+import { useForm, type UseFormReturn } from 'react-hook-form'
+import { singlePageApplicationFormSchema, type SinglePageApplicationFormSchema } from './schema'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 type SinglePageApplicationFormContext = {
   step: number
   incrementStep: () => void
   decrementStep: () => void
+  useSinglePageApplicationForm: UseFormReturn<SinglePageApplicationFormSchema>
 }
 
 export const SinglePageApplicationFormContext = createContext<SinglePageApplicationFormContext>(
   {} as SinglePageApplicationFormContext,
 )
 
+type UseFormOptionProperty<T, K extends string> = T extends Partial<{ [P in K]: infer V }> ? V : never
+type UseFormOption = Parameters<typeof useForm<SinglePageApplicationFormSchema>>['0']
+type FormDefaultValue = UseFormOptionProperty<UseFormOption, 'defaultValues'>
+
+const formDefaultValue: () => FormDefaultValue = () => ({
+  email: '',
+  name: '',
+  gender: undefined,
+  phoneNumbers: ['', '', ''],
+  bio: '',
+  emailSubscription: true,
+  newEmailsSubscribed: true,
+  marketingEmailsSubscribed: true,
+  password: '',
+  passwordConfirmation: '',
+})
+
 export const singlePageApplicationFormContextDefaultValue: () => SinglePageApplicationFormContext = () => {
   const [step, setStep] = useState(1)
   const incrementStep = () => setStep((s) => Math.min(s + 1, 4))
   const decrementStep = () => setStep((s) => Math.max(s - 1, 1))
 
-  return { step, incrementStep, decrementStep }
+  const useSinglePageApplicationForm = useForm<SinglePageApplicationFormSchema>({
+    resolver: zodResolver(singlePageApplicationFormSchema),
+    mode: 'onChange',
+    defaultValues: formDefaultValue(),
+  })
+
+  return { step, incrementStep, decrementStep, useSinglePageApplicationForm }
 }
