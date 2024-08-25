7c151344c3b61943dcadb5a932862a229bb008e8
qounme <qounme@gmail.com>
2024-08-25 17:24:02 +0900

    [issue/5] feat: 都道府県を返すエンドポイントを実装する

--- src/configs/routes.ts
+++ src/configs/routes.ts
@@ -1,5 +1,8 @@
 export const routes = {
   root: '/',
+  api: {
+    prefectures: '/api/prefectures.json',
+  },
   frameworks: {
     react: {
       singlePageApplicationForm: '/frameworks/react/single-page-application-form',
