7931546ae48265dc7238247fe20272033480e5ab
qounme <qounme@gmail.com>
2024-08-30 23:50:14 +0900

    [issue/5] feat: エラーメッセージを変更する

--- src/functions/frameworks/react/SinglePageApplicationForm/schema.ts
+++ src/functions/frameworks/react/SinglePageApplicationForm/schema.ts
@@ -12,26 +12,26 @@ const validatePassword = (data: { password: string; passwordConfirmation: string
 
 const emailFormSchema = () =>
   z.object({
-    email: z.string().regex(EMAIL_REGEX, { message: 'Please enter your email.' }),
+    email: z.string().regex(EMAIL_REGEX, { message: 'メールアドレスを入力してください' }),
   })
 const profileFormSchema = () =>
   z.object({
-    name: z.string().min(1, { message: 'Please enter your name.' }),
+    name: z.string().min(1, { message: '名前を入力してください' }),
     gender: z.enum(['male', 'female', 'non-binary', 'undisclosed']),
     phoneNumbers: z
       .tuple([z.string(), z.string(), z.string()])
-      .refine(validatePhoneNumber, { message: 'Please enter your phone number.', path: [''] }),
-    prefecture: z.string().min(1, { message: 'Please select the prefecture.' }),
-    bio: z.string().max(BIO_LENGTH_LIMIT, { message: `Your bio must be within ${BIO_LENGTH_LIMIT} characters.` }),
-    hobbies: z.array(z.object({ value: z.string() })).max(3, { message: 'Hobbies can be entered up to three.' }),
+      .refine(validatePhoneNumber, { message: '携帯電話番号を入力してください', path: [''] }),
+    prefecture: z.string().min(1, { message: '都道府県を選択してください' }),
+    bio: z.string().max(BIO_LENGTH_LIMIT, { message: 'プロフィールは20文字以内で入力してください' }),
+    hobbies: z.array(z.object({ value: z.string() })).max(3, { message: '趣味は3個まで入力できます' }),
     emailSubscription: z.boolean(),
     newEmailsSubscribed: z.boolean(),
     marketingEmailsSubscribed: z.boolean(),
   })
 const passwordFormSchema = () =>
   z.object({
-    password: z.string().regex(PASSWORD_REGEX, { message: 'Please enter the password.' }),
-    passwordConfirmation: z.string().min(1, { message: 'Please enter the same password again.' }),
+    password: z.string().regex(PASSWORD_REGEX, { message: 'パスワードを入力してください' }),
+    passwordConfirmation: z.string().min(1, { message: '同じパスワードを入力してください' }),
   })
 
 export const emailFormProperties = emailFormSchema().keyof()._def.values
@@ -41,6 +41,6 @@ export const passwordFormProperties = passwordFormSchema().keyof()._def.values
 export const singlePageApplicationFormSchema = emailFormSchema()
   .merge(profileFormSchema())
   .merge(passwordFormSchema())
-  .refine(validatePassword, { message: 'Please enter the same password again.', path: ['passwordConfirmation'] })
+  .refine(validatePassword, { message: '同じパスワードを入力してください', path: ['passwordConfirmation'] })
 
 export type SinglePageApplicationFormSchema = z.infer<typeof singlePageApplicationFormSchema>
